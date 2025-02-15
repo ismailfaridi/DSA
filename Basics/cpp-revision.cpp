9e09b04f5ddc78ad2fae6bd5e81d721c4e0f2514
Muhammad Ismail <112934517+ismailfaridi@users.noreply.github.com>
2025-02-15 21:25:22 +0500

    Create cpp-revision.cpp

---
+++ Basics/cpp-revision.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h> // include every standard library
+using namespace std;
+
+int main()
+{
+    // # IO
+    // std::cout << "Hello World!" << std::endl;
+    // cout << "Hello World!" << endl;
+
+    // int n;
+    // cout << "Enter number: ";
+    // cin >> n;
+    // cout << "You enter: " << n;
+
+
+    // # Data types: int, long, long long, float, double, string (getline), char, bool, ...
+
+    // string s1, s2;
+    // cin >> s1 >> s2; // separate the space
+    // cout << s1 << " " << s2;
+
+    // string str;
+    // getline(cin, str);
+    // cout << str;
+
+    // char ch='i';
+    // cout << ch;
+
+
+    // # If-else Statement
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // if(age < 18){
+    //     cout << "You are underage.";
+    // }
+    // else if(age > 80){
+    //     cout << "You are overage.";
+    // }
+    // else{
+    //     cout << "You are allowed to drive car.";
+    // }
+
+
+    // # Switch Statement
+    // int day;
+    // cout << "Enter the day number (1-7): ";
+    // cin >> day;
+
+    // switch(day){
+    //     case 1:
+    //         cout << "Monday";
+    //         break;
+    //     case 2:
+    //         cout << "Tuesday";
+    //         break;
+    //     case 3:
+    //         cout << "Wednesday";
+    //         break;
+    //     case 4:
+    //         cout << "Thursday";
+    //         break;
+    //     case 5:
+    //         cout << "Friday";
+    //         break;
+    //     case 6:
+    //         cout << "Saturday";
+    //         break;
+    //     case 7:
+    //         cout << "Sunday";
+    //         break;
+    //     default:
+    //         cout << "Invalid number is entered.";
+    // }
+
+
+    // # Array (store similar data, consecutive mempry location modifiable, 0 based indexixng)
+    // $ 1-Dimension Array
+    // int arr[5];
+
+    // for(int i=0; i<5; i++){
+    //     arr[i] = i + 1;
+    // }
+
+    // arr[0] += 100;
+
+    // for(int i=0; i<5; i++){
+    //     cout << arr[i] << endl;
+    // }
+
+    // cout << "Starting address of array: " << arr;
+
+    // $ 2-Dimension Array
+    // int arr[3][5]; // rows, cols
+    // arr[1][3] = 786;
+    // cout << arr[1][3];
+
+
+    // # String
+    // string str ="Ismail";
+    // cout << str[0] << endl;
+    // cout << str[5] << endl;
+
+    // cout << "sizeof() = " << sizeof(str) << endl; // memory size of string object, compile time
+    // cout << "size() = " << str.size() << endl; // number of characters in the string, run time
+    // cout << "length() = " << str.length() << endl; // number of characters in the string
+
+    // str[str.size() - 1] = 'X';
+    // cout << str[5];
+
+
+    // # for loop
+    return 0;
+}
