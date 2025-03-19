e45b044df653a1f566c22b25580cd948814f4f63
Muhammad Ismail <contact@ismailfaridi.com>
2025-03-19 17:36:39 +0500

    leetcode: marjority element

--- Basics/Data Structures/04-Majority-Element.cpp
+++ Basics/Data Structures/04-Majority-Element.cpp
@@ -12,8 +12,8 @@ The majority element is the element that appears more than ⌊n / 2⌋ times. Yo
 # Moore's Voting Algorithm
 - Same element: freq++
 - Diff elment: freq--
-- At last we the largest frequency elment dominate.
-=> Must have a majority element.
+- At last the largest frequency element dominate.
+=> Array must have a majority element.
 
 TC: O(n)
 SC: O(1)
@@ -63,6 +63,7 @@ int optimize(vector<int> nums)
     return ans;
 }
 
+// Moore's Voting Algorithm
 // O(n)
 int moores_voting_algo(vector<int> nums)
 {
@@ -84,13 +85,49 @@ int moores_voting_algo(vector<int> nums)
     return ans;
 }
 
+// If ME exist return that element, otherwise return -1
+// Moore's Voting Algorithm + sum more cases
+int majority_element_variation(vector<int> nums)
+{
+    int freq = 0, ans = 0, n = nums.size(), count = 0;
+
+    for(int i=0; i<n; i++) {
+        if(freq == 0) {
+            ans = nums[i];
+        }
+
+        if(nums[i] == ans) {
+            freq++;
+        }
+        else {
+            freq--;
+        }
+    }
+
+    for(auto i : nums) {
+        if(i == ans){
+            count++;
+        }
+    }
+
+    if(count > n/2) {
+        return ans;
+    }
+    else {
+        return -1;
+    }
+}
+
 int main()
 {
     vector<int> vec = {1, 2, 2, 1, 1};
+    vector<int> vecV = {1, 2, 3};
 
     cout << "Majority Element: " << brute_force(vec) << endl;
     cout << "Majority Element: " << optimize(vec) << endl;
     cout << "Majority Element: " << moores_voting_algo(vec) << endl;
+    
+    cout << "Majority Element: " << majority_element_variation(vecV) << endl;
 }
 
 /*
